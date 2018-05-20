52cbc9fe9cf8de4dd554ab526453a975572fb885
Steven <sunetwli102@gmail.com>
2018-05-19 22:34:33 -0500

    array

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef uint32_t int32;
 
 int main(void) {
-	int a;
-	int b = 10;
-	float c = 5.5;
+	int32 a[4];
+	a[0] = 3;
+	a[1] = 2;
+	a[2] = 4;
+	a[3] = 5;
 
-	printf("a adress is %x\n", &a);
-	printf("b adress is %x\n", &b);
-	printf("c adress is %x\n", &c);
+	int32 b[4] = {5,6,7,8};
+	printf("a values are %d, %d, %d, %d\n", a[0], a[1], a[2], a[3]);
 
-	int *p1;
-	p1 = &a;
 
-	int *p2 = &b;
-	float *p3 = &c;
-
-	printf("p1 value is %x\n", p1);
-	printf("p1 value is %d\n", *p1);
-	printf("p2 value is %x\n", p2);
-	printf("p2 value is %d\n", *p2);
-	printf("p3 value is %x\n", p3);
-	printf("p3 value is %f\n", *p3);
+	printf("b values are %d, %d, %d, %d\n", b[0], b[1], b[2], b[3]);
 	return 0;
 }
