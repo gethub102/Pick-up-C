4c42b0634abab690d15173291e5deb84c0bc8e5c
Steven <sunetwli102@gmail.com>
2018-05-19 21:28:00 -0500

    pointers

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-
 
 int main(void) {
-	char message[12] = "Hello world";
+	int a;
+	int b = 10;
+	float c = 5.5;
 
-	printf("message is %s\n", message);
+	printf("a adress is %u\n", &a);
+	printf("b adress is %u\n", &b);
+	printf("c adress is %u\n", &c);
 
-	printf("11th char is %c\n", message[11]);
-	printf("length of message is %zu\n", strlen(message));
+	int *p1;
+	p1 = &a;
+
+	int *p2 = &b;
+	float *p3 = &c;
+
+	printf("p1 value is %u\n", p1);
+	printf("p1 value is %d\n", *p1);
+	printf("p2 value is %u\n", p2);
+	printf("p2 value is %d\n", *p2);
+	printf("p3 value is %u\n", p3);
+	printf("p3 value is %f\n", *p3);
 	return 0;
 }
