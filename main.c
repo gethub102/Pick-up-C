1f4d8f73e282f62ff8f01b01a55180b5bf456eea
Steven <sunetwli102@gmail.com>
2018-05-19 22:26:33 -0500

    nothind

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main(void) {
 	int b = 10;
 	float c = 5.5;
 
-	printf("a adress is %u\n", &a);
-	printf("b adress is %u\n", &b);
-	printf("c adress is %u\n", &c);
+	printf("a adress is %x\n", &a);
+	printf("b adress is %x\n", &b);
+	printf("c adress is %x\n", &c);
 
 	int *p1;
 	p1 = &a;
@@ -15,11 +15,11 @@ int main(void) {
 	int *p2 = &b;
 	float *p3 = &c;
 
-	printf("p1 value is %u\n", p1);
+	printf("p1 value is %x\n", p1);
 	printf("p1 value is %d\n", *p1);
-	printf("p2 value is %u\n", p2);
+	printf("p2 value is %x\n", p2);
 	printf("p2 value is %d\n", *p2);
-	printf("p3 value is %u\n", p3);
+	printf("p3 value is %x\n", p3);
 	printf("p3 value is %f\n", *p3);
 	return 0;
 }
