3317aedfabdaa19a6d3b20532a0115f9d3b13b2c
Steven <sunetwli102@gmail.com>
2018-05-20 00:27:33 -0500

    source files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Pick-up-C
+
+```
+gcc -c main.c
+gcc -c addnumbers
+gcc -o foo main.o addnumbers.o
+
+```
+
+run the code:
+``` ./foo ```
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-	printf("argc = %d\n", argc);
-	for (int i = 0; i < argc; i++) {
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+int addnumbers(int a, int b);
+int main(void) {
+	int total = 0;
+	total = addnumbers(2, 3);
+	printf("total is %d\n", total);
 	return 0;
 }
