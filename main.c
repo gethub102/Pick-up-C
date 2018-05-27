7a528abc381ff95b374c68fa1293c5922d4d967c
Steven <sunetwli102@gmail.com>
2018-05-27 00:09:39 -0500

    trick value

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
-	char str[24] = "First string";
+	char str[13] = "First string";
 	char *ptr = "Second string";
 
 	printf("str = %s\n", str);
 	printf("ptr = %s\n", ptr);
 
+	printf("str len = %lu\n", strlen(str));
 
 	ptr++;
 	printf("ptr %s\n", ptr);
 
 	char * pstr = (char*)(&str + 1); // forward a whole string
-	printf("str - 1 = %s\n", *(pstr - 2));
+	printf("str - 1 = %c\n", *(pstr - 2));
 
 	str[1] = 'a';
-	printf("str = %s\n", str);
+	printf("str = %c\n", *(str + 1));
 
 	/*****this will cause bus error*****/
 	*(ptr + 1) = 'a'; 
